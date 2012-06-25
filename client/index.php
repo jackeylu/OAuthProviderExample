f7477dfb5cc75d73e40f10ecc55bb4349567fa47
jackeylu <lvlin@whu.edu.n>
2012-06-25 15:24:29 +0800

    added some more debug informaton

--- client/index.php
+++ client/index.php
@@ -3,11 +3,19 @@
 	$oauth_client->enableDebug();
 	try {
 		$info = $oauth_client->getRequestToken("http://www.oape.net/oauth/request_token?oauth_callback=http://www.oape.net/client/callback.php");
+		if (FALSE == $info || !isset($info) || empty($info))
+		{
+			print "Failed fetching request token, response was:".
+				$oauth_client->getLastResponse();
+			exit(1);
+		}
 		echo "<h1>We have a request token !</h1>";
 		echo "<strong>Request token</strong> : ".$info['oauth_token']."<br />";
 		echo "<strong>Request token secret</strong> : ".$info['oauth_token_secret']."<br />";
 		echo "to authenticate go <a href=\"".$info['authentification_url']."?oauth_token=".$info['oauth_token']."\">here</a>";
 	} catch(OAuthException $E){
+		echo "<pre>OAuthException occured.</pre>";
+		echo "<pre>".print_r($E->getTrace(),true)."</pre>";
 		echo "<pre>".print_r($E->debugInfo,true)."</pre>";
 	}
 ?>

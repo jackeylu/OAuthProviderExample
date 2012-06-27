eb79839a560924fd63572a5ddcc01bcd18da91a1
jackeylu <lvlin@whu.edu.n>
2012-06-27 22:10:13 +0800

    modified with url macros

--- client/getuid.php
+++ client/getuid.php
@@ -1,9 +1,10 @@
 <?php
+require_once(dirname(__FILE__) . "config.php");
+
 $access_token		= '8cc17dc4f26f78c18699f0bbccd16f3b5e232811';
 $access_token_secret	= '1c0a400a911574dec8ac7d8f594904aadd6fda17';
 $consumer_key		= 'key';
 $consumer_secret	= 'secret';
-$sp_api_user		= 'http://192.168.127.149/OAuthProviderExample/oauth/?api/user';
 
 try{
 	$oauth_client  = new Oauth($consumer_key,
@@ -11,8 +12,8 @@ try{
 	$oauth_client->enableDebug();
 	$oauth_client->setToken($access_token,
 			$access_token_secret);
-	$oauth_client->fetch($sp_api_user);
-	echo "API RESULT : " . $oauth_client->getLastResponse();
+	$oauth_client->fetch($API_USER_ID_URL);
+	echo "user id with given access token $access_token is : " . $oauth_client->getLastResponse();
 }catch(OAuthException $e){
 	echo "get userid failed with an OAuthException: ";
 	echo $e->getMessage();
--- client/index.php
+++ client/index.php
@@ -2,7 +2,7 @@
 	$oauth_client = new Oauth("key","secret");
 	$oauth_client->enableDebug();
 	try {
-		$info = $oauth_client->getRequestToken("http://192.168.127.149/OAuthProviderExample/oauth/?request_token&oauth_callback=http://192.168.127.149/OAuthProviderExample/client/callback.php");
+		$info = $oauth_client->getRequestToken($REQUEST_TOKEN_URL . "&oauth_callback=" . $CALLBACK_URL);
 		if (FALSE == $info || !isset($info) || empty($info))
 		{
 			print "Failed fetching request token, response was:".
@@ -12,7 +12,7 @@
 		echo "<h1>We have a request token !</h1>";
 		echo "<strong>Request token</strong> : ".$info['oauth_token']."<br />";
 		echo "<strong>Request token secret</strong> : ".$info['oauth_token_secret']."<br />";
-		echo "to authenticate go <a href=\""."http://192.168.127.149/OAuthProviderExample/oauth/login.php"."?oauth_token=".$info['oauth_token']."\">here</a>";
+		echo "to authenticate go <a href=\"".$SERVER_LOGIN_URL."?oauth_token=".$info['oauth_token']."\">here</a>";
 	} catch(OAuthException $E){
 		echo "<pre>OAuthException occured.</pre>";
 		echo "<pre>". $E->getMessage() . "</pre>";
